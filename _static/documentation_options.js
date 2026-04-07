0bdb8ea743070b36c596b1a38c0cf0c6a04d4d66
WOWWA1 <WOWWA1@users.noreply.github.com>
2026-04-07 14:20:23 +0000

    deploy: 4a0299b3fe4fb7650c8f499658d8a153e76c29d6

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.0.post1.dev603+g19a8b21',
+    VERSION: '0.0.post1.dev602+g4a0299b',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
